f5e173e5b0e9caa09a552144563c21f003f67227
murfel <murfel@users.noreply.github.com>
2020-05-14 16:23:39 +0300

    expand dataset downloading to all 12 DIMACS instances

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-wget -O rome99.gr http://users.diag.uniroma1.it/challenge9/data/rome/rome99.gr
-cat rome99.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > rome.in
-rm rome99.gr
+function download {
+    wget -O USA-road-d.$1.gr.gz http://users.diag.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.$1.gr.gz
+    gunzip USA-road-d.$1.gr.gz
+    cat USA-road-d.$1.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > $1.in
+    rm USA-road-d.$1.gr
+}
 
-wget -O USA-road-d.NY.gr.gz http://users.diag.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.NY.gr.gz
-gunzip USA-road-d.NY.gr.gz
-cat USA-road-d.NY.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > ny.in
-rm USA-road-d.NY.gr
+#wget -O rome99.gr http://users.diag.uniroma1.it/challenge9/data/rome/rome99.gr
+#cat rome99.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > rome.in
+#rm rome99.gr
+
+for NAME in NY BAY COL FLA NW NE CAL LKS E W CTR USA
+do
+  download $NAME
+done
