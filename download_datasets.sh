461402c59a371148bd40658692224ed7c9cf5bb0
murfel <murfel@users.noreply.github.com>
2020-05-14 14:37:07 +0300

    add NY (n=200k, m=700k, 2.5 MB) dataset

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 wget -O rome99.gr http://users.diag.uniroma1.it/challenge9/data/rome/rome99.gr
-cat rome99.gr | tail -n +15 | cut -c 3- | tail -c +4 > rome.in
+cat rome99.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > rome.in
 rm rome99.gr
+
+wget -O USA-road-d.NY.gr.gz http://users.diag.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.NY.gr.gz
+gunzip USA-road-d.NY.gr.gz
+cat USA-road-d.NY.gr | sed -e '/^c/d' | cut -c 3- | tail -c +4 > ny.in
+rm USA-road-d.NY.gr
