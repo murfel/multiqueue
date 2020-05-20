36a3c3b67c6cac94b0509cda6433a293bd4a77f1
murfel <murfel@users.noreply.github.com>
2020-05-20 18:06:59 +0300

    make num_non_empty_queues atomic

--- multiqueue.h
+++ multiqueue.h
@@ -50,8 +50,7 @@ template<class T>
 class Multiqueue {
 private:
     std::vector<LockablePriorityQueueWithEmptyElement<T>> queues;
-    std::size_t num_non_empty_queues = 0;
-    std::mutex num_non_empty_queues_mutex;
+    std::atomic<int> num_non_empty_queues;
     T empty_element;
     std::random_device dev;
     std::mt19937 rng;
@@ -61,7 +60,8 @@ private:
     }
 public:
     Multiqueue(int num_threads, int size_multiple, T empty_element) :
-            empty_element(empty_element), rng(dev()), dist(0, std::max(2, num_threads * size_multiple) - 1) {
+            num_non_empty_queues(0), empty_element(empty_element), rng(dev()),
+            dist(0, std::max(2, num_threads * size_multiple) - 1) {
         std::size_t num_queues = std::max(2, num_threads * size_multiple);
         queues.reserve(num_queues);
         for (std::size_t i = 0; i < num_queues; i++) {
@@ -72,18 +72,14 @@ public:
         auto & queue = queues[gen_random_queue_index()];
         std::lock_guard<std::mutex> lock(queue.mutex);
         if (queue.top() == empty_element) {
-            std::lock_guard<std::mutex> num_lock(num_non_empty_queues_mutex);
             num_non_empty_queues++;
         }
         queue.push(value);
     }
     T pop() {
         while (true) {
-            {
-                std::lock_guard<std::mutex> lock(num_non_empty_queues_mutex);
-                if (num_non_empty_queues == 0) {
-                    return empty_element;
-                }
+            if (num_non_empty_queues == 0) {
+                return empty_element;
             }
 
             std::size_t i, j;
@@ -109,7 +105,6 @@ public:
                 lock1.unlock();
                 T e = q2.pop();
                 if (q2.top() == empty_element) {
-                    std::lock_guard<std::mutex> lock(num_non_empty_queues_mutex);
                     num_non_empty_queues--;
                 }
                 return e;
@@ -117,7 +112,6 @@ public:
                 lock2.unlock();
                 T e = q1.pop();
                 if (q1.top() == empty_element) {
-                    std::lock_guard<std::mutex> lock(num_non_empty_queues_mutex);
                     num_non_empty_queues--;
                 }
                 return e;
