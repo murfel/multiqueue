c5e9c1ddb526eafca8d3e955a5320aff1827d713
murfel <murfel@users.noreply.github.com>
2021-04-21 12:01:33 +0200

    remove AbstractQueue to avoid virtual method resolution

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -112,7 +112,7 @@ std::vector<Implementation> create_impls(const std::vector<std::pair<int, int>>&
         int num_threads = param.first;
         int size_multiple = param.second;
         const QueueFactory multi_queue_factory = [num_threads, size_multiple, one_queue_reserve_size]()
-                { return std::make_unique<MultiQueue<QueueElement>>
+                { return std::make_unique<Multiqueue<QueueElement>>
                 (num_threads, size_multiple, EMPTY_ELEMENT, one_queue_reserve_size); };
         std::string impl_name = std::to_string(num_threads) + " " + std::to_string(size_multiple);
         impls.emplace_back([num_threads, multi_queue_factory] (const AdjList & graph)
--- src/dijkstra.h
+++ src/dijkstra.h
@@ -47,86 +47,6 @@ public:
 
 using AdjList = std::vector<std::vector<Edge>>;
 
-
-template <class T>
-class AbstractQueue {
-public:
-    virtual void push(T elem) = 0;
-    virtual std::size_t get_num_pushes() {
-        return 0;
-    }
-    virtual std::vector<std::size_t> get_max_queue_sizes() {
-        return std::vector<std::size_t>();
-    }
-    virtual T pop() = 0;
-    virtual ~AbstractQueue() = default;
-};
-
-template <class T>
-class RegularPriorityQueue : public AbstractQueue<T> {
-private:
-    std::priority_queue<T> queue;
-    const T empty_element;
-public:
-    explicit RegularPriorityQueue(T empty_element) : empty_element(empty_element) {}
-    void push(T elem) override {
-        queue.push(elem);
-    }
-    T pop() override {
-        if (queue.empty()) {
-            return empty_element;
-        }
-        T elem = queue.top();
-        queue.pop();
-        return elem;
-    }
-};
-
-template <class T>
-class BlockingQueue : public AbstractQueue<T> {
-private:
-    std::priority_queue<T> queue;
-    std::mutex mutex;
-    const T empty_element;
-public:
-    explicit BlockingQueue(T empty_element) : empty_element(empty_element) {}
-    void push(T elem) override {
-        std::lock_guard<std::mutex> lock(mutex);
-        queue.push(elem);
-    }
-    T pop() override {
-        std::lock_guard<std::mutex> lock(mutex);
-        if (queue.empty()) {
-            return empty_element;
-        }
-        T elem = queue.top();
-        queue.pop();
-        return elem;
-    }
-};
-
-template <class T>
-class MultiQueue : public AbstractQueue<T> {
-private:
-    Multiqueue<T> queue;
-    T empty_element;
-public:
-    MultiQueue(const int num_threads, const int size_multiple, T empty_element, std::size_t one_queue_reserve_size) :
-            queue(num_threads, size_multiple, empty_element, one_queue_reserve_size), empty_element(empty_element) {}
-    void push(T elem) override {
-        queue.push(elem);
-    }
-    std::size_t get_num_pushes() override {
-        return queue.get_num_pushes();
-    }
-    std::vector<std::size_t> get_max_queue_sizes() override {
-        return queue.get_max_queue_sizes();
-    }
-    T pop() override {
-        return queue.pop();
-    }
-};
-
 class QueueElement {
 private:
     Vertex vertex;
@@ -150,7 +70,7 @@ public:
     }
 };
 
-using QueueFactory = std::function<std::unique_ptr<AbstractQueue<QueueElement>>()>;
+using QueueFactory = std::function<std::unique_ptr<Multiqueue<QueueElement>>()>;
 
 static const DistType EMPTY_ELEMENT_DIST = -1;
 static const QueueElement EMPTY_ELEMENT = {0, EMPTY_ELEMENT_DIST};
@@ -183,7 +103,7 @@ public:
     }
 };
 
-void thread_routine(const AdjList & graph, AbstractQueue<QueueElement> & queue, AtomicDistVector & dists,
+void thread_routine(const AdjList & graph, Multiqueue<QueueElement> & queue, AtomicDistVector & dists,
         AtomicDistVector & vertex_pull_counts, bool collect_statistics) {
     while (true) {
         QueueElement elem = queue.pop();
@@ -240,7 +160,7 @@ SsspDijkstraDistsAndStatistics calc_sssp_dijkstra(const AdjList & graph, std::si
         const QueueFactory & queue_factory, Vertex start_vertex = 0) {
     std::size_t num_vertexes = graph.size();
     auto queue_ptr = queue_factory();
-    AbstractQueue<QueueElement> & queue = *queue_ptr;
+    Multiqueue<QueueElement> & queue = *queue_ptr;
     queue.push({start_vertex, 0});
     AtomicDistVector atomic_dists = initialize_atomic_vector(num_vertexes, std::numeric_limits<int>::max());
     atomic_dists[0].first = 0;
